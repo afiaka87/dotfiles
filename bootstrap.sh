cdbd1ba9476e5858e3d3702339f4d875c4ee5dca
= <claymullis@gmail.com>
2017-08-10 14:07:29 -0500

    Deleted bloat. Started bootstrap dotfiles script.

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+
+## Jupyter Vim Extension
+# Create required directory in case (optional)
+mkdir -p $(jupyter --data-dir)/nbextensions
+# Clone the repository
+cd $(jupyter --data-dir)/nbextensions
+git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+# Activate the extension
+jupyter nbextension enable vim_binding/vim_binding
+sudo pip3 install jupyter_contrib_nbextensions
+jupyter contrib nbextension install --user
